3e92ba743347ab1cd655586c3a155accfda769a7
Jesús Millán <jgmc3012@gmail.com>
2022-03-03 23:22:15 -0400

    How to run app

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ pip install -r requirements.txt
 
 ## Run Scraper
 ### Open Browser
+
+If you are using WSL see the next [post](https://virtualizationreview.com/articles/2017/02/08/graphical-programs-on-windows-subsystem-on-linux.aspx) and [this](https://code.luasoftware.com/tutorials/x-server/xming-client-4-rejected-from-ip/) if you hace any problem with the permissions with Xming
 ```sh
 # Use python 3.8(equal dockerfile)
 source .envs/.local/.postgres
--- console.py
+++ console.py
@@ -5,11 +5,17 @@ from packages.core.modules import ModuleManager
 from packages.core.utils.logger import Logger
 from packages.core.db import init_db
 from tortoise import run_async
+from socket import gaierror
+from clikit.args.argv_args import ArgvArgs
 
 if __name__ == '__main__':
     application = Application("Commands", 0.1, complete=True)
     AppLoop()
     ModuleManager().import_commands(application)
     Logger()
-    AppLoop().get_loop().run_until_complete(init_db())
+    try:
+        AppLoop().get_loop().run_until_complete(init_db())
+    except gaierror as e:
+        if 'pyppeteer:open_browser' not in ArgvArgs().tokens:
+            raise e 
     application.run()
