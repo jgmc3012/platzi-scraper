5019e4cd95958fa43a32e79e379b942d6fafe3f3
Jesús Millán <jgmc3012@gmail.com>
2022-03-24 11:45:00 -0400

    :recycle: Pattern match in avatar

--- packages/users/utils.py
+++ packages/users/utils.py
@@ -10,9 +10,11 @@ def get_username_from_profile_path(path_profile:str):
 def get_username_from_avatar(avatar_url:str):
     """
     Transform "https://static.platzi.com/media/avatars/alan-isaac_vazquez_807714c1-eccc-4a11-8fd2-780569392832" to "alan-isaac_vazquez"
+              "https://static.platzi.com/media/avatars/avatars/OscarAn-ca26ab2b-2e05-4d57-acc9-88182cb85dc7.png" to "OscarAn"
     """
-    match = re.match(r'.*/(.*)_.*', avatar_url)
-    if match:
-        return match.group(1)
+    for pattenr in [r'.*\/(.*)_.*', r'.*\/(.*)-.*-.*-.*-.*-.*']:
+        match = re.match(pattenr, avatar_url)
+        if match:
+            return match.group(1)
 
     raise ValueError(f"Cant get username from avatar url: {avatar_url}")
